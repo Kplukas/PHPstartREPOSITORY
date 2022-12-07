cd955bc8687f4448066b6405201aa4a5fcfada6f
Kplukas <p.kristupas.p@gmail.com>
2022-12-07 18:23:15 +0200

    PHP string tasks 3-5

--- stringai/stringai.php
+++ stringai/stringai.php
@@ -14,3 +14,15 @@ $b = 'Pavardis';
 echo strtoupper($a),' ', strtolower($b);
 // 3.
 echo '<br>';
+$c = $a.$b;
+echo $c;
+// 4.
+echo '<br>';
+echo mb_substr($a, -3), mb_substr($b, -3);
+// 5.
+echo '<br>';
+$d = str_ireplace('a', '*', 'An American in Paris');
+echo $d;
+//6.
+echo '<br>';
+$e = 'An American in Paris';
