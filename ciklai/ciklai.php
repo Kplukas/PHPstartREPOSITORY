2a514bacf230fe7910b366afd13c6b342bd807f9
Kplukas <p.kristupas.p@gmail.com>
2022-12-09 15:43:39 +0200

    PHP loops task 1.b.

--- ciklai/ciklai.php
+++ ciklai/ciklai.php
@@ -8,3 +8,11 @@ while (strlen($str) < 400){
 };
 echo $div1, $str, $div2;
 // 1. b.
+$str2 = '';
+ while (strlen($str2) < 428){
+    $str2 .= '*';
+    if (strlen($str2) == 50 || strlen($str2) == 104 || strlen($str2) == 158 || strlen($str2) == 212 || strlen($str2) == 266 || strlen($str2) == 320 || strlen($str2) == 374){
+        $str2 .= '<br>';
+    }
+};
+echo $str2;
