de9d89077ffe2fddc7876a155db26e98b7e0cf8c
Kplukas <p.kristupas.p@gmail.com>
2022-12-10 16:43:38 +0200

    PHP loops task 7 complete with 2 variant

--- ciklai/ciklai.php
+++ ciklai/ciklai.php
@@ -92,28 +92,45 @@ for($i = 0; $a != true; $i++){
 echo $sk;
 echo '<br>';
 // 7.
-$petras = rand(10, 20);
-$kazys = rand(5, 25);
+$petras = 0;
+$kazys = 0;
 $petrosuma = 0;
 $kaziosuma = 0;
-do {
-    echo 'Petro taškai:', $petras, ' ';
-    echo 'Kazio taškai:', $kazys;
-    echo '<br>';
-    $petrosuma = $petrosuma + $petras;
-    $kaziosuma = $kaziosuma + $kazys;
-    if($petras > $kazys) {
-        echo 'Partiją laimėjo: Petras';
-    } else if ($kazys > $petras) {
-        echo 'Partiją laimėjo: Kazys';
-    } else {
-        echo 'Lygiosios.';
+for ($i = 0; $i != true; $i++) {
+    if ($petrosuma >= 222 || $kaziosuma >= 222){
+        $i = true;
     }
-    if ($petrosuma >= 222){
-        echo '<br>', 'Laimėjo Petras';
-        echo 'Taškai:', $petrosuma;
-    } else if ($kaziosuma >= 222){
-        echo '<br>', 'Laimėjo Kazys';
-        echo 'Taškai:', $kaziosuma;
-    }
-} while ($petrosuma < 222 || $kaziosuma < 222);
+    while ($petrosuma < 222 || $kaziosuma < 222) {
+        $petras = rand(10, 20);
+        $kazys = rand(5, 25);
+        $petrosuma = $petrosuma + $petras;
+        $kaziosuma = $kaziosuma + $kazys;
+        if($petras > $kazys) {
+            echo 'Partiją laimėjo: Petras. ';
+            echo 'Petro taškai:', $petras, ' ';
+            echo 'Kazio taškai:', $kazys;
+            echo '<br>';
+        } else if ($kazys > $petras) {
+            echo 'Partiją laimėjo: Kazys. ';
+            echo 'Petro taškai:', $petras, ' ';
+            echo 'Kazio taškai:', $kazys;
+            echo '<br>';
+        } else {
+            echo 'Lygiosios. ';
+            echo 'Petro taškai:', $petras, ' ';
+            echo 'Kazio taškai:', $kazys;
+            echo '<br>';
+        }
+        if ($petrosuma >= 222 || $kaziosuma >= 222){
+            if ($petrosuma > $kaziosuma) {
+                echo '<br>', 'Laimėjo Petras. ';
+                echo 'Taškai:', $petrosuma;
+                echo '<br>';
+            } else if ($kaziosuma > $petrosuma){
+                echo '<br>', 'Laimėjo Kazys. ';
+                echo 'Taškai:', $kaziosuma;
+                echo '<br>';
+            }
+        }  
+    };
+};
