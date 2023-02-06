6d1dabcfb62de99ab164f3ecec9e3180904cffa8
Kplukas <p.kristupas.p@gmail.com>
2023-02-06 14:52:15 +0200

    Countries and hotel list style

--- Travel/resources/views/front/country/index.blade.php
+++ Travel/resources/views/front/country/index.blade.php
@@ -5,17 +5,23 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Countries</div>
-
-                <div class="card-body">
+                <div class="card-header text-white bg-dark c-head">Countries</div>
+                <ul class="card-body list-group text-white bg-secondary">
                     @forelse($c as $country)
-                    <h2> {{$country->title}}</h2>
-                    <p>{{$country->season_start}}</p>
-                    <p>{{$country->season_end}}</p>
+                    <li class="list-group-item bg-dark text-white m-3">
+                        <h2 class="c-name"> {{$country->title}}</h2>
+                        <div class="col-6">
+                            <p>Season start: {{$country->season_start}}</p>
+                            <p>Season end: {{$country->season_end}}</p>
+                        </div>
+                        <a class="col-4 btn btn-info" href="{{route('h-index2')}}?filter={{$country->id}}">Show hotels</a>
+                    </li>
                     @empty
-                    <h2>No destinations</h2>
+                    <li class="list-group-item list-group-item-dark">
+                        <h2 class="c-name">No destinations</h2>
+                    </li>
                     @endforelse
-                </div>
+                </ul>
             </div>
         </div>
     </div>
--- Travel/resources/views/front/hotel/index.blade.php
+++ Travel/resources/views/front/hotel/index.blade.php
@@ -5,41 +5,51 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Hotels</div>
-                <form class="card-header" action="{{route('h-index2')}}">
-                    <select class="form-select " aria-label="Default select example" name="filter">
+                <div class="card-header text-white bg-dark c-head">Hotels</div>
+                <form class="card-header  text-white bg-dark c-head" action="{{route('h-index2')}}">
+                    <select class="form-select" aria-label="Default select example" name="filter">
                         <option @if($request->filter == 'all') selected @endif value="all">All</option>
                         @foreach($countries as $country)
                         <option @if($request->filter == $country->id) selected @endif value="{{$country->id}}">{{$country->title}}</option>
                         @endforeach
                     </select>
                     <input type="text" class="form-control col-5 mt-3" style="width: 50%; display:inline" name="search" value="{{$request->search ?? ''}}">
-                    <button class="btn btn-secondary m-1" type="submit">Filter</button>
+                    <button class="btn btn-secondary mt-1 ms-1" type="submit">Filter</button>
                 </form>
-                <div class="card-body">
+                <div class="card-body text-white bg-secondary">
                     <ul class="list-group list-group-flush">
                         @forelse($hotels as $hotel)
-                        <li class="list-group-item">
-                            <h2> {{$hotel->name}},
+                        <li class="list-group-item bg-dark text-white m-3">
+                            <h2 class="c-name"> {{$hotel->name}},
                                 @foreach($countries as $country)
                                 @if ($country->id == $hotel->c_id)
                                 {{$country->title}}
                                 @endif
                                 @endforeach
                             </h2>
-                            <p>{{$hotel->visit_start}}</p>
-                            <p>{{$hotel->visit_end}}</p>
-                            <p>{{$hotel->price}} Eur</p>
-                            <form action="{{route('o-store')}}" method="post">
-                                <input type="hidden" name="client_id" value="{{Auth::user()->id}}">
-                                <input type="hidden" name="hotel_id" value="{{$hotel->id}}">
-                                <button type="submit" class="btn btn-secondary">Add</button>
-                                @csrf
-                            </form>
+                            <div class="col-6 d-ib">
+                                <p>Visit start: {{$hotel->visit_start}}</p>
+                                <p>Visit end: {{$hotel->visit_end}}</p>
+                                <p>For: {{$hotel->price}} &euro;</p>
+                                <form action="{{route('o-store')}}" method="post">
+                                    <input type="hidden" name="client_id" value="{{Auth::user()->id}}">
+                                    <input type="hidden" name="hotel_id" value="{{$hotel->id}}">
+                                    <button type="submit" class="btn btn-secondary">Add</button>
+                                    @csrf
+                                </form>
+                            </div>
+                            <div class="col-6 d-ib">
+                                @if(!$hotel->photo)
+                                <img src="https://cdn-icons-png.flaticon.com/512/7985/7985827.png" class="d-block w-50" alt="{{$hotel->name}}">
+                                @else
+                                <img src="{{asset($hotel->photo)}}" class="d-block w-100" alt="{{$hotel->name}}">
+                                @endif
+                            </div>
                         </li>
                         @empty
-                        <li class="list-group-item">
-                            <h2>No hotels</h2>
+                        <li class="list-group-item  bg-dark text-white m-3">
+                            <h2 class="c-name">No hotels</h2>
+                            <p>Please select other deals or contact us for information of upcoming hotel deals</p>
                         </li>
                         @endforelse
                     </ul>
