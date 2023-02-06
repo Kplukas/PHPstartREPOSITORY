27a37465edb56b007d63e6213f71e76368948281
Kplukas <p.kristupas.p@gmail.com>
2023-02-06 15:26:54 +0200

    Orders and home page css

--- Travel/resources/views/front/orders/index.blade.php
+++ Travel/resources/views/front/orders/index.blade.php
@@ -5,40 +5,40 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">My orders</div>
-
-                <div class="card-body">
+                <div class="card-header text-white bg-dark c-head">My orders</div>
+                <div class="card-body  text-white bg-secondary">
                     @forelse($orders as $order)
-                    <h2>
-                        @foreach($hotels as $hotel)
-                        @if($hotel->id == $order->hotel_id)
-                        Hotel: {{$hotel->name}}
-                        @endif
-                        @endforeach
-                        @if($order->situation == 'Awaiting confirmation')
-                        <span class="badge rounded-pill bg-secondary">{{$order->situation}}</span>
-                        @elseif($order->situation == 'Cancelled')
-                        <span class="badge rounded-pill bg-danger">{{$order->situation}}</span>
-                        @elseif($order->situation == 'Reviewing')
-                        <span class="badge rounded-pill bg-info text-dark">{{$order->situation}}</span>
-                        @else
-                        <span class="badge rounded-pill bg-success">{{$order->situation}}</span>
-                        @endif
-                    </h2>
-                    <p>
-                        @foreach($hotels as $hotel)
-                        @if($hotel->id == $order->hotel_id)
+                    <div class="bg-dark text-white m-3 p-3">
+                        <h2 class="c-name">
+                            @foreach($hotels as $hotel)
+                            @if($hotel->id == $order->hotel_id)
+                            Hotel: {{$hotel->name}}
+                            @endif
+                            @endforeach
+                            @if($order->situation == 'Awaiting confirmation')
+                            <span class="badge rounded-pill bg-secondary">{{$order->situation}}</span>
+                            @elseif($order->situation == 'Cancelled')
+                            <span class="badge rounded-pill bg-danger">{{$order->situation}}</span>
+                            @elseif($order->situation == 'Reviewing')
+                            <span class="badge rounded-pill bg-info text-dark">{{$order->situation}}</span>
+                            @else
+                            <span class="badge rounded-pill bg-success">{{$order->situation}}</span>
+                            @endif
+                        </h2>
+                        <p>
+                            @foreach($hotels as $hotel)
+                            @if($hotel->id == $order->hotel_id)
 
-                        @foreach($countries as $country)
-                        @if($hotel->c_id == $country->id)
-                        {{$country->title}} || {{$hotel->price}} &euro;
-                        @endif
-                        @endforeach
-                    </p>
-                    <p>
-                        Visit from {{$hotel->visit_start}} to {{$hotel->visit_end}}.
-
-                    </p>
+                            @foreach($countries as $country)
+                            @if($hotel->c_id == $country->id)
+                            {{$country->title}} || {{$hotel->price}} &euro;
+                            @endif
+                            @endforeach
+                        </p>
+                        <p>
+                            Visit from {{$hotel->visit_start}} to {{$hotel->visit_end}}.
+                        </p>
+                    </div>
                     @endif
                     @endforeach
                     @empty
--- Travel/resources/views/home.blade.php
+++ Travel/resources/views/home.blade.php
@@ -4,8 +4,8 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <h2>Welcome!</h2>
-            <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+            <h2 class="card-header text-white bg-dark c-head pt-3">Welcome!</h2>
+            <div id="carouselExampleCaptions" class="carousel slide home-slide" data-bs-ride="carousel">
                 <div class="carousel-indicators">
                     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
